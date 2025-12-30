d6d3fd911a0c0fb7a0e7cedfa31204b53a95fe8d
Tathagata51 <98935535+Tathagata51@users.noreply.github.com>
2025-12-30 20:59:50 +0000

    variables

---
+++ 01_basic/01_variable/01_variable.js
@@ -0,0 +1,15 @@
+const accountId=14453
+let accountEMail="tathagata@magic.com"
+var accountPassword="12345"
+accountCity="Mumbai"
+let accountState
+
+// accountId=2 //not allowed
+console.log(accountId);
+console.table([accountId, accountEMail, accountPassword, accountCity,accountState])
+
+accountEMail="tathagata@supermagic.com"
+accountPassword="54321"
+accountCity="Kolkata"
+
+console.table([accountId, accountEMail, accountPassword, accountCity,accountState])
