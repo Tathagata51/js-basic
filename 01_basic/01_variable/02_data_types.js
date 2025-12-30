fd47f2016f788f4b0f625e6357c3bef7b0b8d17c
Tathagata51 <98935535+Tathagata51@users.noreply.github.com>
2025-12-30 21:17:56 +0000

    data types

---
+++ 01_basic/01_variable/02_data_types.js
@@ -0,0 +1,16 @@
+"use strict";
+
+let name="Tathagata"
+let age=27
+let isStudent=true
+let height=5.9
+let hobbies=["reading", "traveling", "coding"]
+let address={street: "123 Main St", city: "New York", zip: "10001"}
+
+console.log(typeof name);      // string
+console.log(typeof age);    // number
+console.log(typeof isStudent); // boolean
+console.log(typeof height);    // number
+console.log(typeof hobbies);   // object (arrays are of type object in JavaScript)
+console.log(typeof address);   // object
+console.log(typeof null);   // object
